22d40c8a2c344366fa4eccc4de3aa92d2717247d
Mariano <marianom.lopez@alumnos.frm.utn.edu.ar>
2025-11-22 13:05:32 -0300

    Primer commit

---
+++ src/main/java/org/example/ExamenMlApplication.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenMlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenMlApplication.class, args);
+	}
+
+}
